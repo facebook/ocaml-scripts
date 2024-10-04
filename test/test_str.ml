25d311341e823e786c7b1b4c8586cf3e41fdca1f
generatedunixname89002005279527 <generatedunixname89002005279527@meta.com>
2024-10-04 09:45:52 -0700

    fbcode/common/ocaml/scripts/test
    Reviewed By: hick209
    
    Differential Revision: D63623854
    
    fbshipit-source-id: f47c9668f76c5bf749105df1597e728f8693a96a

--- test/test_str.ml
+++
@@ -1,5 +0,0 @@
-let () =
-  Printf.printf "%s\n"
-    (Str.replace_first
-       (Str.regexp {|hello \([A-Za-z]+\)|})
-       {|\1|} "hello world")
--- test/test_unix.ml
+++
@@ -1,5 +0,0 @@
-let _ =
-  try Printf.printf "Hello '%s'!\n" (Unix.getlogin ()) with
-  | Unix.Unix_error (Unix.ENOENT, _, _) -> (
-      try Printf.printf "Hello '%d'!\n" (Unix.getpid ()) with _ -> ())
-  | _ -> ()
